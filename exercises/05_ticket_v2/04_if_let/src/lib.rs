a9b80e6daf796f6d91113abda769714940455a12
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2025-11-17 15:48:57 +0100

    feat(solve): 05_ticket_v2/04_if_let

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -8,7 +8,16 @@ impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        todo!()
+        // let Shape::Circle { radius } = &self else {
+        //     panic!("Only Circles have radii!")
+        // };
+        // radius
+
+        if let Shape::Circle { radius } = self {
+            *radius
+        } else {
+            panic!("Only Circles have radii!")
+        }
     }
 }
 
